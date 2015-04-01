53d9c590b23dd0884645fc65d8938fc6f1b52951
AbiSri <AbiSri@bergen.org>
2015-04-01 10:30:37 -0400

    Displays Text when clicked

--- app/src/main/java/com/abi/sagar/listview/MainActivity.java
+++ app/src/main/java/com/abi/sagar/listview/MainActivity.java
@@ -4,9 +4,14 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+    TextView colorText;
 
     String color_names[] = {"Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Gray", "White"};
     Integer image_id[] = {R.drawable.color_black, R.drawable.color_brown, R.drawable.color_red, R.drawable.color_orange, R.drawable.color_yellow, R.drawable.color_green, R.drawable.color_blue, R.drawable.color_violet, R.drawable.color_grey, R.drawable.color_white};
@@ -15,12 +20,19 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        colorText = (TextView) findViewById(R.id.color_text);
 
         Customlistadapter adapter = new Customlistadapter(this, image_id, color_names);
 
-        ListView lv = (ListView) findViewById(R.id.listView);
+        ListView lv = (ListView) findViewById(R.id.listView1);
         lv.setAdapter(adapter);
         lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                colorText.setText(color_names[position]);
+            }
+        });
 
 
         /*int height = lv.getHeight();
